14e7b7ed81ff2990bc5fa3b01fbfea532b67cb75
Jordan <Jordan.Brockopp@target.com>
2021-04-16 11:00:26 -0500

    docs: add clarifying comments for api version

--- docker/docker.go
+++ docker/docker.go
@@ -4,8 +4,21 @@
 
 package docker
 
-// Version represents the supported
-// Docker API version for the mock.
+// nolint: godot // ignore comment ending in a list
+//
+// Version represents the supported Docker API version for the mock.
+//
+// The Docker API version is pinned to ensure compatibility between the
+// Docker API and client. The goal is to maintain n-1 compatibility.
+//
+// The maximum supported Docker API version for the client is here:
+//
+// https://docs.docker.com/engine/api/#api-version-matrix
+//
+// For example (use the compatibility matrix above for reference):
+//
+// * the Docker version of v20.10 has a maximum API version of v1.41
+// * to maintain n-1, the API version is pinned to v1.40
 const Version = "v1.40"
 
 // New returns a client that is capable of handling
