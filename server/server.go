7c31308f19fd96787e6eaeec423aea4ddb4c9071
Kelly Merrick <kelly.merrick@target.com>
2021-10-01 13:59:18 -0500

    feat(log streaming): add mocks for step and service (#128)
    * feat(log streaming): add mocks for step and service
    
    * remove unnecessary lines

--- server/server.go
+++ server/server.go
@@ -98,6 +98,7 @@ func FakeHandler() http.Handler {
 	e.POST("/api/v1/repos/:org/:repo/builds/:build/steps", addStep)
 	e.PUT("/api/v1/repos/:org/:repo/builds/:build/steps/:step", updateStep)
 	e.DELETE("/api/v1/repos/:org/:repo/builds/:build/steps/:step", removeStep)
+	e.POST("/api/v1/repos/:org/:repo/builds/:build/steps/:step/stream", postStepStream)
 
 	// mock endpoints for service calls
 	e.GET("/api/v1/repos/:org/:repo/builds/:build/services/:service", getService)
@@ -105,6 +106,7 @@ func FakeHandler() http.Handler {
 	e.POST("/api/v1/repos/:org/:repo/builds/:build/services", addService)
 	e.PUT("/api/v1/repos/:org/:repo/builds/:build/services/:service", updateService)
 	e.DELETE("/api/v1/repos/:org/:repo/builds/:build/services/:service", removeService)
+	e.POST("/api/v1/repos/:org/:repo/builds/:build/services/:service/stream", postServiceStream)
 
 	// mock endpoints for user calls
 	e.GET("/api/v1/users/:user", getUser)
